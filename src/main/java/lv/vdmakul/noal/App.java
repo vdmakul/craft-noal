23818a7e2851b592bc24a028a71418cc211ffeb7
Vadim Akula <vdmakul@gmail.com>
2014-09-30 23:16:41 +0300

    initial Spring web project

---
+++ src/main/java/lv/vdmakul/noal/App.java
@@ -0,0 +1,11 @@
+package lv.vdmakul.noal;
+
+import lv.vdmakul.noal.config.WebApp;
+import org.springframework.boot.SpringApplication;
+
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApp.class, args);
+    }
+}
